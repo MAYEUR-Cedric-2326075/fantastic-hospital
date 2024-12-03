b640fd8c4be100cce183cf27c8865e71499230c2
MaximeDELIND21218703 <98822652+DELIN-Maxime-2225028a@users.noreply.github.com>
2024-12-03 15:50:36 +0100

    Added maxCapacity in MedicalService.java

--- src/main/java/fantasticHospital/Hospital/MedicalService/MedicalService.java
+++ src/main/java/fantasticHospital/Hospital/MedicalService/MedicalService.java
@@ -14,21 +14,28 @@ public class MedicalService implements Randomizer {
 
     private String name;
     private int budget;
+    private int maxCapacity;
     private HashSet<CreatureSickness> patients = new HashSet<>();
 
     public MedicalService(String name){
         this.name = name;
         this.budget = 100000;
+        this.maxCapacity = 10;
     }
+    
     public MedicalService(){
         this.name = "NoName";
         this.budget = 100000;
+        this.maxCapacity = 10;
     }
+    
     public int size(){return this.patients.size();}
+    
     public MedicalService(String name,int budget){
         this.name = name;
         this.budget = budget;
     }
+    
     public boolean isEmpty(){
         return patients.isEmpty();
     }
@@ -36,20 +43,39 @@ public class MedicalService implements Randomizer {
     public String getName() {
         return name;
     }
+    
     public boolean isCrypt(){return false;}
+    
     public boolean isQuarantine(){return false;}
+    
     public void setName(String name) {this.name=name;}
+    
     public int getBudget() {return budget;}
+    
     public void setBudget(int budget) {this.budget = budget;}
+    
     public HashSet<CreatureSickness> getPatients() {return patients;}
+    
     public void addPatient(CreatureSickness creatureSickness){patients.add(creatureSickness);}
+    
     public void removePatient(CreatureSickness creatureSickness){patients.remove(creatureSickness);}
+    
     public void reviewBudget(int budget){this.budget =getBudget()-budget;}
+    
     public CreatureSickness getRandomPatient() {
         if (patients.isEmpty())
             return null;
         return patients.stream().findFirst().orElse(null);
     }
+    
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+    
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity=maxCapacity;
+    }
+    
     //public HashSet getPatients(){}
     public void waiting(){
         for (CreatureSickness creatureSickness : patients) {
@@ -65,6 +91,7 @@ public class MedicalService implements Randomizer {
             }
         }
     }
+    
     public HashSet<Creature> getCreatures() {
         HashSet<Creature> creatures = new HashSet<>();
         for (CreatureSickness creatureSickness : patients) {
@@ -72,6 +99,7 @@ public class MedicalService implements Randomizer {
         }
         return creatures;
     }
+    
     @Override
     public String toString(){return "String de servide";}
 
